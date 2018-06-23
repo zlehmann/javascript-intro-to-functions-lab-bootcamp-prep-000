c80919bf6e0346a9b3d4e86a52e2fd0580320bd5
zlehmann@gmail.com <zlehmann@gmail.com>
2018-06-23 13:58:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,10 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if string.toLowerCase === string {
-    return 
+  if (string.toLowerCase === string) {
+    return "I can\'t hear you!"
+  }
+  if (string.toUpperCase === string) {
+    return ""
   }
 }
